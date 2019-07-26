bc1611a1a120e85d344eae6ba253f5fda147e575
Lorensov <lucianlorens@gmail.com>
2019-07-26 02:31:25 -0300

    :wrench: :bug:

--- api/server.py
+++ api/server.py
@@ -17,7 +17,7 @@ from classifier import anomaly_classifier
 import json
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, origins="*")
 app.config['CORS_HEADERS'] = 'Content-Type'
 swagger = Swagger(app, config=swagger_configuration)
 
@@ -34,14 +34,14 @@ def decode_image(file):
 
 @app.route('/', methods=['GET'])
 @app.route('/status', methods=['GET'])
-@cross_origin
+@cross_origin()
 @swag_from('swagger/status.yml')
 def status():
     return jsonify({'status': 'ok'})
 
 
 @app.route('/keystroke', methods=['POST'])
-@cross_origin
+@cross_origin()
 @swag_from('swagger/keystroke.yml')
 def keystroke():
     req_dict = request.get_json()
@@ -52,7 +52,7 @@ def keystroke():
 
 
 @app.route('/face', methods=['POST'])
-@cross_origin
+@cross_origin()
 @swag_from('swagger/face.yml')
 def face_recognition():
     if not 'image' in request.files:
@@ -68,6 +68,7 @@ def face_recognition():
     return jsonify({'classification': classification})
 
 @app.route('/anomaly', methods=['POST'])
+@cross_origin()
 @swag_from('swagger/anomaly.yml')
 def anomaly_classify():
 
